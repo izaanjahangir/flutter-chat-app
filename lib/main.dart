b0d3e2b61e3958dd476c20ed06abbc7de3e63183
izaanjahangir <izaanjahangir2@gmail.com>
2022-01-26 20:06:21 +0500

    added basic firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:chat_app/screens/home/home.dart';
 import 'package:chat_app/screens/login/login.dart';
 import 'package:chat_app/screens/register/register.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
